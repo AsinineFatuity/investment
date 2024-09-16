18e1710b806da2b464c1e337806db9bfaca7638d
Kembo Franklyne <asininefatuity@gmail.com>
2024-09-17 01:55:56 +0300

    wrote refresh token viewset

--- core/viewsets/refresh.py
+++ core/viewsets/refresh.py
@@ -4,11 +4,11 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework.permissions import AllowAny
 from rest_framework import status
-from rest_framework import viewsets
+from rest_framework.viewsets import ViewSet
 from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 
 
-class RefreshViewSet(viewsets.ViewSet, TokenRefreshView):
+class RefreshViewSet(ViewSet, TokenRefreshView):
     permission_classes = (AllowAny,)
     authentication_classes = []
     http_method_names = ["post"]
