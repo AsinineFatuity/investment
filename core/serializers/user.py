6f9c3a8bf339f31cfab2d5434a134715594b4eac
Kembo Franklyne <asininefatuity@gmail.com>
2024-09-16 23:57:05 +0300

    override create user in serializer

--- core/serializers/user.py
+++ core/serializers/user.py
@@ -1,5 +1,6 @@
 from core.models import User
 from core.serializers.abstract import AbstractSerializer
+from core.permissions import CreatePermission
 
 
 class UserSerializer(AbstractSerializer):
@@ -7,3 +8,8 @@ class UserSerializer(AbstractSerializer):
         model = User
         fields = ["id", "username", "email", "first_name", "last_name"]
         read_only_field = ["is_active", "id"]
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        CreatePermission(user).add_user_to_groups()
+        return super().create(validated_data)
