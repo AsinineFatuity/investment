618a2a32c9184f2b15d09fbed0ac4ece13525fc2
Kembo Franklyne <asininefatuity@gmail.com>
2024-09-17 23:28:00 +0300

    added viewset to urls

--- core/routers.py
+++ core/routers.py
@@ -1,9 +1,17 @@
 from rest_framework import routers
-from core.viewsets import RegisterViewSet, LoginViewSet, RefreshViewSet
+from core.viewsets import (
+    RegisterViewSet,
+    LoginViewSet,
+    RefreshViewSet,
+    ViewOnlyTransactionViewSet,
+)
 
 router = routers.SimpleRouter()
 
 router.register(r"auth/register", RegisterViewSet, basename="register")
 router.register(r"auth/login", LoginViewSet, basename="login")
 router.register(r"auth/refresh", RefreshViewSet, basename="refresh")
+router.register(
+    r"transaction/view-only", ViewOnlyTransactionViewSet, basename="view-only"
+)
 urlpatterns = [*router.urls]
--- core/viewsets/__init__.py
+++ core/viewsets/__init__.py
@@ -1,3 +1,4 @@
 from core.viewsets.auth.register import RegisterViewSet
 from core.viewsets.auth.login import LoginViewSet
 from core.viewsets.auth.refresh import RefreshViewSet
+from core.viewsets.transaction.view_only import ViewOnlyTransactionViewSet
