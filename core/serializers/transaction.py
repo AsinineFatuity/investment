828eda4c2742325f325b0a58d9a20f744071624e
Kembo Franklyne <asininefatuity@gmail.com>
2024-09-17 23:14:17 +0300

    abstracted redundancy

--- core/serializers/transaction.py
+++ core/serializers/transaction.py
@@ -1,20 +1,26 @@
 from rest_framework import serializers
 from core.models import AllPermTransaction, PostOnlyTransaction, ViewOnlyTransaction
+from core.serializers.abstract import AbstractSerializer
 
 
-class AllPermTransactionSerializer(serializers.ModelSerializer):
+class BaseTransactionSerializer(AbstractSerializer):
     class Meta:
+        fields = ["id", "amount", "date", "type", "created_at", "updated_at"]
+
+
+class AllPermTransactionSerializer(BaseTransactionSerializer):
+    class Meta(BaseTransactionSerializer.Meta):
         model = AllPermTransaction
         read_only_fields = ["id", "created_at", "updated_at"]
 
 
-class PostOnlyTransactionSerializer(serializers.ModelSerializer):
-    class Meta:
+class PostOnlyTransactionSerializer(BaseTransactionSerializer):
+    class Meta(BaseTransactionSerializer.Meta):
         model = PostOnlyTransaction
         read_only_fields = ["id", "created_at", "updated_at"]
 
 
-class ViewOnlyTransactionSerializer(serializers.ModelSerializer):
-    class Meta:
+class ViewOnlyTransactionSerializer(BaseTransactionSerializer):
+    class Meta(BaseTransactionSerializer.Meta):
         model = ViewOnlyTransaction
         read_only_fields = ["id", "created_at", "updated_at"]
