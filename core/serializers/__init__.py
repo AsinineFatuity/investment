170c30b77efa22a52b506dcf8e9855f50d598fe7
Kembo Franklyne <asininefatuity@gmail.com>
2024-09-17 23:16:40 +0300

    modularized viewsets

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,3 +1,8 @@
 from core.serializers.user import UserSerializer
 from core.serializers.register import RegisterSerializer
 from core.serializers.login import LoginSerializer
+from core.serializers.transaction import (
+    AllPermTransactionSerializer,
+    PostOnlyTransactionSerializer,
+    ViewOnlyTransactionSerializer,
+)
--- core/viewsets/__init__.py
+++ core/viewsets/__init__.py
@@ -1,3 +1,3 @@
-from core.viewsets.register import RegisterViewSet
-from core.viewsets.login import LoginViewSet
-from core.viewsets.refresh import RefreshViewSet
+from core.viewsets.auth.register import RegisterViewSet
+from core.viewsets.auth.login import LoginViewSet
+from core.viewsets.auth.refresh import RefreshViewSet
