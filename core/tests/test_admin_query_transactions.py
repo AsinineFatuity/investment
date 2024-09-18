0f1e5224d8da43861bf5dcf9ec3e0059cb69bb3d
Kembo Franklyne <asininefatuity@gmail.com>
2024-09-18 10:26:40 +0300

    improved test

--- core/tests/test_admin_query_transactions.py
+++ core/tests/test_admin_query_transactions.py
@@ -24,14 +24,16 @@ class TestAdminQueryTransactions(CustomTestCase):
         self.create_transaction_data()
 
     def create_transaction_data(self):
+        withdrawal_amount = 200
+        deposit_amount = 1000
         withdrawal_data = {
-            "amount": 200,
+            "amount": withdrawal_amount,
             "type": TransactionTypeEnum.W,
             "user": self.user,
             "date": "2024-09-01",
         }
         deposit_data = {
-            "amount": 1000,
+            "amount": deposit_amount,
             "type": TransactionTypeEnum.D,
             "user": self.user,
             "date": "2024-09-01",
@@ -46,8 +48,11 @@ class TestAdminQueryTransactions(CustomTestCase):
             all_to_create.append(
                 AllPermTransaction(account=self.all_perm_account, **data)
             )
-        PostOnlyTransaction.objects.bulk_create(post_to_create)
-        AllPermTransaction.objects.bulk_create(all_to_create)
+        post_only_transactions = PostOnlyTransaction.objects.bulk_create(post_to_create)
+        all_perm_transactions = AllPermTransaction.objects.bulk_create(all_to_create)
+        self.expected_account_balance = deposit_amount - withdrawal_amount
+        self.total_post_transactions = len(post_only_transactions)
+        self.total_all_perm_transactions = len(all_perm_transactions)
 
     def test_non_admin_user_cannot_query_transactions(self):
         response = self.client.get(self.admin_query_transaction_url)
